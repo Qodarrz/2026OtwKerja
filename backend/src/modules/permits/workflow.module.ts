139a38c07c68e5b81e7aa4ef2cb1a0a9a1737b2a
OwlDane <zidunnoaoe@gmail.com>
2026-04-22 14:38:24 +0700

    feat(transparency): implement public transparency dashboard with status tracking

--- backend/src/modules/permits/workflow.module.ts
+++ backend/src/modules/permits/workflow.module.ts
@@ -5,14 +5,26 @@ import { NotificationModule } from './notification.module';
 import { WorkflowService } from './services/workflow.service';
 import { SLAService } from './services/sla.service';
 import { AnalyticsService } from './services/analytics.service';
+import { TransparencyService } from './services/transparency.service';
 import { WorkflowController } from './controllers/workflow.controller';
 import { SLAController } from './controllers/sla.controller';
 import { AnalyticsController } from './controllers/analytics.controller';
+import { TransparencyController } from './controllers/transparency.controller';
 
 @Module({
   imports: [PrismaModule, PermitModule, NotificationModule],
-  controllers: [WorkflowController, SLAController, AnalyticsController],
-  providers: [WorkflowService, SLAService, AnalyticsService],
-  exports: [WorkflowService, SLAService, AnalyticsService],
+  controllers: [
+    WorkflowController,
+    SLAController,
+    AnalyticsController,
+    TransparencyController,
+  ],
+  providers: [
+    WorkflowService,
+    SLAService,
+    AnalyticsService,
+    TransparencyService,
+  ],
+  exports: [WorkflowService, SLAService, AnalyticsService, TransparencyService],
 })
 export class WorkflowModule {}
