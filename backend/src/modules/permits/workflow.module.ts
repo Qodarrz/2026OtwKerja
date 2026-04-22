fbe3aadef3d83ef4ab24776f13b0c6c983db86ca
OwlDane <zidunnoaoe@gmail.com>
2026-04-22 14:41:59 +0700

    feat(reporting): implement advanced reporting with trends and bottleneck analysis

--- backend/src/modules/permits/workflow.module.ts
+++ backend/src/modules/permits/workflow.module.ts
@@ -6,10 +6,12 @@ import { WorkflowService } from './services/workflow.service';
 import { SLAService } from './services/sla.service';
 import { AnalyticsService } from './services/analytics.service';
 import { TransparencyService } from './services/transparency.service';
+import { ReportingService } from './services/reporting.service';
 import { WorkflowController } from './controllers/workflow.controller';
 import { SLAController } from './controllers/sla.controller';
 import { AnalyticsController } from './controllers/analytics.controller';
 import { TransparencyController } from './controllers/transparency.controller';
+import { ReportingController } from './controllers/reporting.controller';
 
 @Module({
   imports: [PrismaModule, PermitModule, NotificationModule],
@@ -18,13 +20,21 @@ import { TransparencyController } from './controllers/transparency.controller';
     SLAController,
     AnalyticsController,
     TransparencyController,
+    ReportingController,
   ],
   providers: [
     WorkflowService,
     SLAService,
     AnalyticsService,
     TransparencyService,
+    ReportingService,
   ],
-  exports: [WorkflowService, SLAService, AnalyticsService, TransparencyService],
+  exports: [
+    WorkflowService,
+    SLAService,
+    AnalyticsService,
+    TransparencyService,
+    ReportingService,
+  ],
 })
 export class WorkflowModule {}
